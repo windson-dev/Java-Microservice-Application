39480ed6800efede6d90983a338f046a0a788f7e
Windson Macedo <windsondev@hotmail.com>
2023-02-01 18:51:17 -0300

    Microsservice AuthUser

---
+++ authuser/src/main/java/com/ead/authuser/AuthuserApplication.java
@@ -0,0 +1,12 @@
+package com.ead.authuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthuserApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AuthuserApplication.class, args);
+	}
+
+}
---
+++ authuser/src/main/java/com/ead/authuser/Repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ead.authuser.Repositories;
+
+import com.ead.authuser.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
