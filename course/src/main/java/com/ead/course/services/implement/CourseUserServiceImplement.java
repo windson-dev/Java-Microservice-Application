c4b051aa0cf6e2384ae5f956b3015b8eed7025e6
Windson Macedo <windsondev@hotmail.com>
2023-02-10 18:31:56 -0300

    feat: Composition pattern and relational mapping for comunication in between microservices

---
+++ course/src/main/java/com/ead/course/services/implement/CourseUserServiceImplement.java
@@ -0,0 +1,8 @@
+package com.ead.course.services.implement;
+
+import com.ead.course.services.CourseUserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseUserServiceImplement implements CourseUserService {
+}
